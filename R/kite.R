b7138b2ef50c11821ec24a2eae2dfee317cd6caf
Kirill Müller <krlmlr+r@mailbox.org>
2022-12-18 21:24:47 +0100

    Distribute

---
+++ R/kite.R
@@ -0,0 +1,27 @@
+#' Krackhardt's kite
+#'
+#' @description
+#'
+#'   Krackhardt's kite is a fictionary social network with ten actors.
+#'   It is the smallest graph for which the most central actor is different
+#'   according to the three classic centality measures: degree, closeness
+#'   and betweenness.
+#'
+#'
+#'
+#' @name kite
+#' @docType data
+#' @usage
+#' kite
+#' @format
+#'   An undirected igraph graph with graph attributes \code{name},
+#'   \code{layout}, \code{Citation}, \code{Author}, \code{URL}, and vertex
+#'   attributes \code{label}, \code{name} and \code{Firstname}.
+#' @references
+#'   Assessing the Political Landscape: Structure, Cognition, and Power in
+#'   Organizations. David Krackhardt. \emph{Admin. Sci. Quart.} 35, 342-369,
+#'   1990.
+#' @source
+#'   \url{http://www.orgnet.com/sna.html}
+#' @keywords datasets
+NULL
