51e38845d666a25619258e6aeaf8ca2c7c0d6054
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-16 06:57:31 +0200

    feat: Upgrade graphs to igraph 1.5.0

---
+++ inst/upgrade.R
@@ -0,0 +1,17 @@
+library(igraph, warn.conflicts = FALSE)
+
+upgrade <- function(x) {
+  if (inherits(x, "igraph")) {
+    upgrade_graph(x)
+  } else {
+    # foodwebs
+    lapply(x, upgrade_graph)
+  }
+}
+
+for (f in fs::dir_ls("data")) local({
+  load(f)
+  print(ls())
+  assign(ls(), upgrade(get(ls())))
+  save(list = ls(), file = f)
+})
