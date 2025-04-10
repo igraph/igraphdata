bc6a34a410ad3987c35fb52d63d59b83ba200465
schochastics <david@schochastics.net>
2025-04-10 22:12:41 +0200

    added slash notation and directed/bipartite checking via metadata

--- R/netzschleuder.R
+++ R/netzschleuder.R
@@ -1,5 +1,5 @@
-download_file <- function(zip_url, token = NULL, file) {
-  req <- httr2::request(zip_url)
+make_request <- function(url, token = NULL) {
+  req <- httr2::request(url)
   req <- httr2::req_throttle(req, capacity = 20, fill_time_s = 60)
 
   if (!is.null(token)) {
@@ -17,35 +17,71 @@ download_file <- function(zip_url, token = NULL, file) {
     stop("Failed to download file. Status: ", httr2::resp_status(resp))
   }
 
-  if (!grepl("application/zip", httr2::resp_content_type(resp))) {
-    warning(
-      "Response does not look like a ZIP file (Content-Type: ",
-      httr2::resp_content_type(resp),
-      ")"
-    )
-  }
+  resp
+}
 
+resolve_name <- function(x) {
+  if (grepl("/", x)) {
+    return(strsplit(x, "/", fixed = TRUE)[[1]])
+  } else {
+    c(x, x)
+  }
+}
+
+download_file <- function(zip_url, token = NULL, file) {
+  resp <- make_request(zip_url, token)
   writeBin(httr2::resp_body_raw(resp), file)
   invisible(NULL)
+}
+
+meta_from_netzschleuder <- function(net_ident, call = rlang::caller_env()) {
+  url <- sprintf("https://networks.skewed.de/api/net/%s", net_ident[1])
+  resp <- make_request(url)
+  raw <- httr2::resp_body_json(resp)
+  if (net_ident[1] == net_ident[2] && length(unlist(raw$nets)) > 1) {
+    cli::cli_abort(
+      c(
+        "{net_ident[1]} is a collection and downloading a whole collection is not permitted.",
+        "i" = "see {.url https://networks.skewed.de/net/{net_ident[1]}}"
+      ),
+      call = call
+    )
+  } else if (net_ident[1] == net_ident[2]) {
+    return(raw)
+  } else {
+    idx <- which(unlist(raw[["nets"]]) == net_ident[2])
+    if (length(idx) == 0) {
+      cli::cli_abort(
+        c(
+          "{net_ident[2]} is not part of the collection {net_ident[1]}.",
+          "i" = "see {.url https://networks.skewed.de/net/{net_ident[1]}}"
+        ),
+        call = call
+      )
+    }
+    raw[["analyses"]] <- raw[["analyses"]][[net_ident[2]]]
+    raw[["nets"]] <- raw[["nets"]][idx]
+    raw
+  }
 }
 
 #' Download a graph from the Netzschleuder data catalogue
 #' Netzschleuder (<https://networks.skewed.de/>) is a large online repository for
 #' network datasets with the aim of aiding scientific research.
-#' @param name character. name of the network dataset.
-#' @param network character. If the dataset contains several networks this is the network name.
+#' @param name character. name of the network dataset. To get a network from a collection, use `<collection_name>/<network_name>`.
 #' @param token character. Some networks have restricted access and need a toke. See <https://networks.skewed.de/restricted>
 #' @return a named list containing an edge list and node attribute data frame and some metadata
 #' @export
-read_from_netzschleuder <- function(name, ..., network = NULL, token = NULL) {
-  if (is.null(network)) {
-    network <- name
-  }
+read_from_netzschleuder <- function(name, token = NULL) {
+  net_ident <- resolve_name(name)
+  meta <- meta_from_netzschleuder(net_ident)
+
   zip_url <- sprintf(
     "https://networks.skewed.de/net/%s/files/%s.csv.zip",
-    name,
-    network
+    net_ident[1],
+    net_ident[2]
   )
+
   temp <- tempfile(fileext = "zip")
   download_file(zip_url, token = token, file = temp)
 
@@ -53,7 +89,7 @@ read_from_netzschleuder <- function(name, ..., network = NULL, token = NULL) {
 
   edge_file_name <- grep("edge", zip_contents$Name, value = TRUE)
   node_file_name <- grep("node", zip_contents$Name, value = TRUE)
-  meta_file_name <- grep("gprops", zip_contents$Name, value = TRUE)
+  gprops_file_name <- grep("gprops", zip_contents$Name, value = TRUE)
 
   edges_df_raw <- utils::read.csv(unz(temp, edge_file_name))
   edges_df <- minty::type_convert(edges_df_raw)
@@ -83,11 +119,11 @@ read_from_netzschleuder <- function(name, ..., network = NULL, token = NULL) {
     nodes_df$y <- y_vals
   }
 
-  meta_df <- utils::read.csv(unz(temp, meta_file_name))
+  gprops_df <- utils::read.csv(unz(temp, gprops_file_name))
 
   on.exit(unlink(temp))
 
-  list(nodes = nodes_df, edges = edges_df, meta = meta_df)
+  list(nodes = nodes_df, edges = edges_df, gprops = gprops_df, meta = meta)
 }
 
 #' Create a graph from the Netzschleuder data catalogue
@@ -95,18 +131,15 @@ read_from_netzschleuder <- function(name, ..., network = NULL, token = NULL) {
 #' Netzschleuder (<https://networks.skewed.de/>) is a large online repository for
 #' network datasets with the aim of aiding scientific research.
 #' @inheritParams read_from_netzschleuder
-#' @param directed logical. Whether a directed graph is constructed.
-#' @param bipartite logical. Whether a bipartite graph is constructed.
 #' @return a new graph object.
 #' @export
 graph_from_netzschleuder <- function(
-  name,
-  ...,
-  network = NULL,
-  directed = FALSE,
-  bipartite = FALSE
+  name
 ) {
-  graph_data <- read_from_netzschleuder(name, network)
+  graph_data <- read_from_netzschleuder(name)
+  directed <- graph_data$meta[["analyses"]][["is_directed"]]
+  bipartite <- graph_data$meta[["analyses"]][["is_bipartite"]]
+
   g <- igraph::graph_from_data_frame(
     graph_data$edges,
     directed = directed,
