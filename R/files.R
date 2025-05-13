a7ec20f0a1f951db139d751ea35ea5ffca622b43
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-05-13 11:56:29 +0200

    docs: add reference for lesmis
    The DOI at https://dl.acm.org/doi/book/10.1145/164984 does not work

--- R/files.R
+++ R/files.R
@@ -5,6 +5,8 @@
 #'
 #' @return A string indicating an absolute path to a file.
 #' @name lesmis
+#' @references D. E. Knuth, The Stanford GraphBase: A Platform for Combinatorial Computing, Addison-Wesley, Reading, MA (1993).
+#' \url{https://www-cs-faculty.stanford.edu/~knuth/sgb.html}
 #' @export
 lesmis_gml <- function() {
   system.file("files/lesmis.gml", package = "igraphdata", mustWork = TRUE)
